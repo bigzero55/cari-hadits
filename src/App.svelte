de62d113d35b667115d32634b1981d472053e19e
Abdul Rojak <abdulrozzaaq5@outlook.co.id>
2023-02-01 10:28:13 +0700

    card terpisah

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,13 @@
 <script>
   //@ts-nocheck
-  import { MaterialApp, AppBar,Button, Icon, ListItem, Menu, TextField, Card, CardText, CardActions } from 'svelte-materialify';
+  import { MaterialApp, Button, TextField, Card, CardText} from 'svelte-materialify';
   import { mdiMenu , mdiDotsVertical} from '@mdi/js'
   import { writable } from 'svelte/store'
+  import HaditsCard from './components/HaditsCard.svelte';
 
   let datahadits = writable()
   
-  let theme = 'light';
   let keys = 'النيات'
-
-  function toggleTheme() {
-    if (theme === 'light') theme = 'dark';
-    else theme = 'light';
-  }
 
   async function check (key) {
     const result = await fetch(`https://dorar-hadith-api.cyclic.app/api/search?value=${key}`)
@@ -27,52 +22,43 @@
 
 </script>
 
-<MaterialApp {theme}>
-  <AppBar>
-    <!-- <div slot="icon">
-      <Button fab depressed>
-        <Icon path={mdiMenu} />
-      </Button>
-    </div> -->
-    <span slot="title">Cari Hadits</span>
-    <div style="flex-grow:1" />
-    <!-- <Button>Item</Button> -->
-    <Menu right>
-      <div slot="activator">
-        <Button fab depressed>
-          <Icon path={mdiDotsVertical} />
-        </Button>
-      </div>
-      <ListItem>Item 1</ListItem>
-      <ListItem>Item 2</ListItem>
-      <ListItem>Item 3</ListItem>
-    </Menu>
-  </AppBar>
-
-  <div class="ma-5">
-    <TextField dense rounded outlined
-    bind:value={keys}
-    >masukan kata kunci</TextField> 
-    <div class="mt-2">
-      <Button on:click={CariHadits} rounded >Cari</Button>
-    </div>
+<MaterialApp>
+  <div class="header">
+    <Card outlined class="pa-0">
+      <CardText class="">
+        <div class="text-center mb-2">
+          <div class="text-h5 font-weight-black">
+            Cari Hadits
+          </div>
+        </div>
+        <div class="pl-4 pr-4 pt-3">
+          <div class="ma-2">
+            <TextField dense rounded outlined
+            class="font-kitab"
+            bind:value={keys}
+            >masukan kata kunci</TextField> 
+            <div class="mt-2">
+              <Button on:click={CariHadits} rounded >Cari</Button>
+            </div>
+          </div>
+        </div>
+      </CardText>
+    </Card>
   </div>
 
   <div class="ma-2">
     {#if $datahadits != undefined}
       {#each $datahadits as hadits}
-        <Card outlined >
-          <div class="pl-4 pr-4 pt-3">
-            <span class="text-overline font-kitab-bold">{hadits.el_mohdith}</span>
-            <br />
-            <span class="text-h5 mb-2 font-kitab-bold">{hadits.el_rawi}</span>
-            <br />
-          </div>
-          <CardText class="font-kitab">
-            {hadits.hadith}
-          </CardText>
-        </Card>
+      <HaditsCard {...hadits}/>
       {/each}
     {/if}
   </div>
 </MaterialApp>
+
+<style>
+  .header {
+    width: 100%;
+    position: fixed;
+    z-index: 1;
+  }
+</style>
---
+++ src/components/HaditsCard.svelte
@@ -0,0 +1,30 @@
+<script>
+  //@ts-nocheck
+  import { Card, CardActions, CardText, Divider } from "svelte-materialify";
+
+   export let el_mohdith =  ''
+   export let el_rawi = ''
+   export let grade = ''
+   export let hadith = ''
+   export let source = ''
+   export let number_or_page = ''
+  
+</script>
+
+<Card outlined style="margin-bottom:10px; padding:5px">
+  <CardText class="font-kitab-bold">
+   {hadith}
+  </CardText>
+  <Divider />
+  <CardActions style="direction:rtl">
+   <div>
+      <p class="font-kitab mb-0">
+         <small>{el_rawi}</small> -
+         <small>{el_mohdith}</small> -
+         <small>{grade}</small> -
+         <small>{source}</small> -
+         <small>{number_or_page}</small>
+      </p>
+   </div>
+  </CardActions>
+</Card>
