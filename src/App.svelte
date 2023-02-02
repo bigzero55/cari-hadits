7a4f3bb27e3bd8e59bdfe6a40040cf0bb1a512aa
Abdul Rojak <abdulrozzaaq5@outlook.co.id>
2023-02-02 15:30:33 +0700

    edit sore

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,23 @@
 <script>
   //@ts-nocheck
   import {
+    Icon,
     MaterialApp,
     Button,
     TextField,
     Card,
     CardText,
+    ProgressCircular,
   } from "svelte-materialify";
-  import { mdiMenu, mdiDotsVertical } from "@mdi/js";
+  import { mdiBookSearchOutline } from "@mdi/js";
   import { writable } from "svelte/store";
   import HaditsCard from "./components/HaditsCard.svelte";
+  import { Clock } from "svelte-loading-spinners";
 
-  let datahadits = writable();
+  let datahadits = writable([]);
 
   let keys = "النيات";
-
+  let load = false;
   async function check(key) {
     const result = await fetch(
       `https://dorar-hadith-api.cyclic.app/api/search?value=${key}`
@@ -24,8 +27,14 @@
     console.log(data);
   }
 
-  function CariHadits() {
-    check(keys);
+  function CariHadits(key) {
+    load = true;
+    check(key);
+    console.log(key);
+  }
+
+  $: if ($datahadits.length > 0) {
+    load = false;
   }
 </script>
 
@@ -34,20 +43,26 @@
     <Card outlined class="">
       <CardText class="">
         <div class="text-center mb-2">
-          <div class="text-h5 font-weight-black">Cari Hadits</div>
+          <h3 class="text-h3 font-weight-black orange-text">Cari Hadits</h3>
         </div>
         <div class="pl-4 pr-4 pt-3">
           <div class="ma-2">
-            <TextField
-              dense
-              rounded
-              outlined
-              class="font-kitab"
-              bind:value={keys}>Masukan Kata Kunci</TextField
-            >
-            <div class="mt-2">
-              <Button on:click={CariHadits} rounded>Cari</Button>
-            </div>
+            <form on:submit|preventDefault={() => CariHadits(keys)}>
+              <TextField
+                clearable 
+                dense
+                rounded
+                outlined
+                class="font-kitab"
+                bind:value={keys}>Masukan Kata Kunci</TextField
+              >
+              <div class="mt-2">
+                <Button type="submit" rounded class="orange white-text">
+                  <Icon path={mdiBookSearchOutline} class="mr-2" />
+                  Cari
+                </Button>
+              </div>
+            </form>
           </div>
         </div>
       </CardText>
@@ -55,10 +70,16 @@
   </div>
 
   <div class="pl-2 pr-2 bod">
-    {#if $datahadits != undefined}
+    {#if $datahadits != []}
       {#each $datahadits as hadits}
         <HaditsCard {...hadits} />
       {/each}
+    {/if}
+
+    {#if load}
+      <div class="loading text-center">
+        <ProgressCircular size={70} width={2} indeterminate color="orange" />
+      </div>
     {/if}
   </div>
 </MaterialApp>
@@ -71,8 +92,14 @@
   }
 
   .bod {
-    padding-top: 200px;
+    padding-top: 220px;
     padding-bottom: 50px;
     background-color: antiquewhite;
+  }
+
+  .loading {
+    margin-top: 200px;
+    width: 100%;
+    align-items: center;
   }
 </style>
--- src/components/HaditsCard.svelte
+++ src/components/HaditsCard.svelte
@@ -12,19 +12,28 @@
 </script>
 
 <Card outlined style="margin-bottom:10px; padding:5px">
-  <CardText class="font-kitab-bold">
-   {hadith}
+  <CardText >
+   <p class="font-kitab-bold hadis">
+      {hadith}
+   </p>
   </CardText>
   <Divider />
   <CardActions style="direction:rtl">
    <div>
-      <p class="font-kitab mb-0">
-         <small>{el_rawi}</small> -
-         <small>{el_mohdith}</small> -
-         <small>{grade}</small> -
-         <small>{source}</small> -
-         <small>{number_or_page}</small>
+      <p class="font-kitab-bold mb-0">
+         <small class="orange-text">{el_rawi}</small> -
+         <small class="red-text">{grade}</small> -
+         <small class="green-text">{el_mohdith}</small> -
+         <small class="brown-text darken-1">{source}</small>
+         <small class="brown-text darken-1">({number_or_page})</small>
       </p>
    </div>
   </CardActions>
 </Card>
+
+<style>
+   .hadis {
+      font-size: 16px;
+      line-height: 25px;
+   }
+</style>
