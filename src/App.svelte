e763854473936574054ae37e38ef4343be7a8f66
Abdul Rojak <abdulrozzaaq5@outlook.co.id>
2023-02-02 10:27:47 +0700

    dgddg

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,52 @@
 <script>
   //@ts-nocheck
-  import { MaterialApp, Button, TextField, Card, CardText} from 'svelte-materialify';
-  import { mdiMenu , mdiDotsVertical} from '@mdi/js'
-  import { writable } from 'svelte/store'
-  import HaditsCard from './components/HaditsCard.svelte';
+  import {
+    MaterialApp,
+    Button,
+    TextField,
+    Card,
+    CardText,
+  } from "svelte-materialify";
+  import { mdiMenu, mdiDotsVertical } from "@mdi/js";
+  import { writable } from "svelte/store";
+  import HaditsCard from "./components/HaditsCard.svelte";
 
-  let datahadits = writable()
-  
-  let keys = 'النيات'
+  let datahadits = writable();
 
-  async function check (key) {
-    const result = await fetch(`https://dorar-hadith-api.cyclic.app/api/search?value=${key}`)
-    const data = await result.json()
-    datahadits.set(data)
-    console.log(data)
+  let keys = "النيات";
+
+  async function check(key) {
+    const result = await fetch(
+      `https://dorar-hadith-api.cyclic.app/api/search?value=${key}`
+    );
+    const data = await result.json();
+    datahadits.set(data);
+    console.log(data);
   }
 
   function CariHadits() {
-    check(keys)
+    check(keys);
   }
-
 </script>
 
 <MaterialApp>
   <div class="header">
-    <Card outlined class="pa-0">
+    <Card outlined class="">
       <CardText class="">
         <div class="text-center mb-2">
-          <div class="text-h5 font-weight-black">
-            Cari Hadits
-          </div>
+          <div class="text-h5 font-weight-black">Cari Hadits</div>
         </div>
         <div class="pl-4 pr-4 pt-3">
           <div class="ma-2">
-            <TextField dense rounded outlined
-            class="font-kitab"
-            bind:value={keys}
-            >masukan kata kunci</TextField> 
+            <TextField
+              dense
+              rounded
+              outlined
+              class="font-kitab"
+              bind:value={keys}>Masukan Kata Kunci</TextField
+            >
             <div class="mt-2">
-              <Button on:click={CariHadits} rounded >Cari</Button>
+              <Button on:click={CariHadits} rounded>Cari</Button>
             </div>
           </div>
         </div>
@@ -46,10 +54,10 @@
     </Card>
   </div>
 
-  <div class="ma-2">
+  <div class="pl-2 pr-2 bod">
     {#if $datahadits != undefined}
       {#each $datahadits as hadits}
-      <HaditsCard {...hadits}/>
+        <HaditsCard {...hadits} />
       {/each}
     {/if}
   </div>
@@ -60,5 +68,11 @@
     width: 100%;
     position: fixed;
     z-index: 1;
+  }
+
+  .bod {
+    padding-top: 200px;
+    padding-bottom: 50px;
+    background-color: antiquewhite;
   }
 </style>
